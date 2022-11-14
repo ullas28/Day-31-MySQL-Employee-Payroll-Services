f79e099bb9f3ab4d97b3e216ce601357785158b4
unknown <you@example.com>
2022-11-14 20:35:42 +0530

    Added : retrieve salary date for particular range or employee

--- Employee Payroll Services.sql
+++ Employee Payroll Services.sql
@@ -6,3 +6,5 @@ use payroll_service;
  insert into employee_payroll (name, salary, start) values ( 'bill',100000.00,'2018-01-03'),
 ('terisa',200000.00,'2019-11-13');
 select * from employee_payroll; 
+select salary from employee_payroll where name='bill'; 
+select * from employee_payroll where start between CAST('2018-01-01' AS DATE)AND DATE(NOW());

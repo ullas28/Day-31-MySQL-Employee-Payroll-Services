c98cd15e525b1fa279c9075df549dcf45cf0cad5
unknown <you@example.com>
2022-11-14 20:22:14 +0530

    Added : created employee payroll table

--- Employee Payroll Services.sql
+++ Employee Payroll Services.sql
@@ -1,3 +1,6 @@
 create database payroll_service;  
 show databases;           
  use payroll_service;              
+ create table employee_payroll (id INT unsigned NOT NULL AUTO_INCREMENT, name VARCHAR(30) NOT NULL,
+ salary Double NOT NULL, start DATE NOT NULL, PRIMARY KEY (id));
+ Select * from payroll_service.employee_payroll;

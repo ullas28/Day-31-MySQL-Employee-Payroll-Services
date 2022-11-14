ec56e982c24d49e325480bee3f02aaf04aaf96e8
unknown <you@example.com>
2022-11-14 20:31:19 +0530

    Added : retreive from employee payroll

--- Employee Payroll Services.sql
+++ Employee Payroll Services.sql
@@ -3,6 +3,6 @@ show databases;
 use payroll_service;              
  create table employee_payroll (id INT unsigned NOT NULL AUTO_INCREMENT, name VARCHAR(30) NOT NULL,
  salary Double NOT NULL, start DATE NOT NULL, PRIMARY KEY (id));
- Select * from payroll_service.employee_payroll;
  insert into employee_payroll (name, salary, start) values ( 'bill',100000.00,'2018-01-03'),
 ('terisa',200000.00,'2019-11-13');
+select * from employee_payroll; 

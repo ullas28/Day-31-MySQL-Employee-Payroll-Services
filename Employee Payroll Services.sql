6e4fffe67171d43327ef6e438221cf88bf61e69d
unknown <you@example.com>
2022-11-17 12:07:20 +0530

    Added : SUM, AVG, MIN, MAX and COUNT

--- Employee Payroll Services.sql
+++ Employee Payroll Services.sql
@@ -11,3 +11,11 @@ select * from employee_payroll where start between CAST('2018-01-01' AS DATE)AND
 alter table employee_payroll add gender char(1) after name; 
 update payroll_service.employee_payroll set gender = 'M' where name = 'bill';
 update payroll_service.employee_payroll set gender = 'F' where name = 'bill';
+insert into payroll_service.employee_payroll (name, gender, salary, start) values ( 'Nitish','M',600000.00,'2018-01-03'),
+('Sarah','F',300000.00,'2019-11-13'), ('Deepika','F',150000.00,'2019-11-13');
+select sum(salary) from payroll_service.employee_payroll where gender = 'F' group by gender;
+select sum(salary) from payroll_service.employee_payroll where gender = 'M' group by gender; 
+select * ,avg(salary) from payroll_service.employee_payroll;
+select * ,min(salary) from payroll_service.employee_payroll;
+select * ,max(salary) from payroll_service.employee_payroll;
+select count(salary) from payroll_service.employee_payroll;

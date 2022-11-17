a11129bddc4ca7301989f52342d87ac80d2e8fb5
unknown <you@example.com>
2022-11-17 12:47:28 +0530

    Added : phone, address and department

--- Employee Payroll Services.sql
+++ Employee Payroll Services.sql
@@ -3,19 +3,10 @@ show databases;
 use payroll_service;              
  create table employee_payroll (id INT unsigned NOT NULL AUTO_INCREMENT, name VARCHAR(30) NOT NULL,
  salary Double NOT NULL, start DATE NOT NULL, PRIMARY KEY (id));
- insert into employee_payroll (name, salary, start) values ( 'bill',100000.00,'2018-01-03'),
-('terisa',200000.00,'2019-11-13');
-select * from employee_payroll; 
-select salary from employee_payroll where name='bill'; 
 select * from employee_payroll where start between CAST('2018-01-01' AS DATE)AND DATE(NOW());
 alter table employee_payroll add gender char(1) after name; 
-update payroll_service.employee_payroll set gender = 'M' where name = 'bill';
-update payroll_service.employee_payroll set gender = 'F' where name = 'bill';
-insert into payroll_service.employee_payroll (name, gender, salary, start) values ( 'Nitish','M',600000.00,'2018-01-03'),
-('Sarah','F',300000.00,'2019-11-13'), ('Deepika','F',150000.00,'2019-11-13');
-select sum(salary) from payroll_service.employee_payroll where gender = 'F' group by gender;
-select sum(salary) from payroll_service.employee_payroll where gender = 'M' group by gender; 
-select * ,avg(salary) from payroll_service.employee_payroll;
-select * ,min(salary) from payroll_service.employee_payroll;
-select * ,max(salary) from payroll_service.employee_payroll;
-select count(salary) from payroll_service.employee_payroll;
+alter table payroll_service.employee_payroll add phone_number varchar(25) after name;
+alter table payroll_service.employee_payroll add address varchar(100) DEFAULT 'TBD' after phone_number;
+alter table payroll_service.employee_payroll add department varchar(150) not null after address;
+select * from payroll_service.employee_payroll;
+Delete from  payroll_service.employee_payroll where department;

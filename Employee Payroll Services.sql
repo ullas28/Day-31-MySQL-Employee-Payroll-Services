b90b0a8da5168edcca88aa8d6640166168803d26
unknown <you@example.com>
2022-11-18 22:55:17 +0530

    Added : implemented ER diagram

--- Employee Payroll Services.sql
+++ Employee Payroll Services.sql
@@ -20,3 +20,10 @@ insert into employee_payroll(id, name, phone_number, address, department, gender
      (122,'terisa','4512474562','TBD','Sales','F',2000000.00,5000000.00,300000.00,400000.00,50000.00,'2019-11-13');
 SELECT * FROM payroll_service.employee_payroll;
 Alter table payroll_service.employee_payroll rename COLUMN id to empid;
+create table employee_department (employee_id VARCHAR(30) NOT NULL, department_id VARCHAR(30));
+insert into employee_payroll(empid, name, phone_number, address, department, gender, basic_pay, deductions, taxablePay, netPay, incomeTax, start) values
+     (123,'Ullas','45124984562','TBD','IT','M',3000000.00,1000000.00,3000000.00,500000.00,50000.00,'2022-11-13');
+insert into employee_payroll(empid, name, phone_number, address, department, gender, basic_pay, deductions, taxablePay, netPay, incomeTax, start) values
+     (124,'Sayyed','451092474562','TBD','HR','M',2000000.00,5000000.00,300000.00,400000.00,50000.00,'2022-11-13');
+insert into employee_department (employee_id, department_id) values (121, 'Marketing'),(122,'Sales'),(123,'IT'),(124,'HR');
+SELECT * FROM payroll_service.employee_department;
